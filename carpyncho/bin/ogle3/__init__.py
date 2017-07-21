014307c27b25d5122831053f02f3a92bed1d9f79
Juan B Cabral <jbc.develop@gmail.com>
2017-07-21 17:49:39 -0300

    new ogle data and preproc ready

--- carpyncho/bin/ogle3/__init__.py
+++ carpyncho/bin/ogle3/__init__.py
@@ -3,6 +3,10 @@
 
 import os
 
+import pandas as pd
+
+from astropy.coordinates import SkyCoord
+
 import sh
 from sh import bzip2
 
@@ -21,8 +25,34 @@ PATH = os.path.abspath(os.path.dirname(__file__))
 # =============================================================================
 
 def build():
+    def unpack():
+        bzip2("-f", "-dk", "ogleIII_all.csv.bz2")
+
+    def fix_pos(odf):
+        ra = odf["RA"].apply(
+                lambda d: d.replace(":", "h", 1).replace(":", "m", 1) + "s")
+        dec = odf["Decl"].apply(
+                lambda d: d.replace(":", "d", 1).replace(":", "m", 1) + "s")
+
+        coords = SkyCoord(ra, dec, frame='icrs')
+        odf['ra_deg'] = pd.Series(coords.ra.deg, index=odf.index)
+        odf['dec_deg'] = pd.Series(coords.dec.deg, index=odf.index)
+        odf["galactic_l"] = pd.Series(coords.galactic.l, index=odf.index)
+        odf["galactic_b"] = pd.Series(coords.galactic.b, index=odf.index)
+        return odf
+
+    def preprocess():
+        df = pd.read_csv("ogleIII_all.csv")
+        df = fix_pos(df)
+        df.to_pickle("ogleIII.pkl")
+
+    def clean():
+        os.remove("ogleIII_all.csv")
+
     with cd(PATH):
-        bzip2("-dk", "ogle3.txt.bz2")
+        unpack()
+        preprocess()
+        clean()
 
 
 def load():
