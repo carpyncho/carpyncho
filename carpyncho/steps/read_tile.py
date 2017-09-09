42030e0b9572e7f0f1f5c7309d014c963550e129
Juan B Cabral <jbc.develop@gmail.com>
2017-09-09 20:36:56 -0300

    read tile now generates version 3.0 of sources id

--- carpyncho/steps/read_tile.py
+++ carpyncho/steps/read_tile.py
@@ -28,6 +28,11 @@ SOURCE_DTYPE = {
 
 USECOLS = [0, 1, 2, 3, 4, 5]
 
+ZONES = {
+    "b": "3",
+    "d": "4"
+}
+
 
 # =============================================================================
 # STEP
@@ -35,7 +40,22 @@ USECOLS = [0, 1, 2, 3, 4, 5]
 
 class ReadTile(run.Step):
     """Convert the tile into a numpy array to be tagged and matched
-    again their pawprints
+    again their pawprints.
+
+    The sources are stored in a numpy record array wiht the orinal data
+    plut the id of every source.
+
+    ### Understanding the Sources ID:
+
+    The id are an 14 digits integer with the format `PTTTOOOOOOOOOO` where:
+
+    - **P:** indicate the position of the tile on the VVV (3=bulge, 4=disc).
+    - **TTT:** Are the tile number of the VVV.
+    - **0000000000:** is a sequential number of the source inside the tile.
+
+    #### Example
+
+    The id "40010000000130" indicate the 130th source inside the tile d001.
 
     """
 
@@ -53,19 +73,24 @@ class ReadTile(run.Step):
         filtered = arr[flt]
         return filtered, len(filtered)
 
-    def add_columns(self, odata, size, tile_id, dtypes):
+    def add_columns(self, odata, size, tile_name, dtypes):
         """Add id to existing recarray
 
         """
 
+        tile_name = ZONES[tile_name[0].lower()] + tile_name[1:]
+
+        def get_id(order):
+            order = str(order).rjust(10, "0")
+            return int(tile_name + order)
+
         # create ids
         ids = np.fromiter(
-            ("tile_{}_{}".format(tile_id, idx + 1) for idx in range(size)),
-            dtype="|S20")
+            (get_id(idx + 1) for idx in range(size)), dtype=np.int64)
 
         dtype = copy.deepcopy(dtypes)
         dtype["names"].insert(0, "id")
-        dtype["formats"].insert(0, "|S20")
+        dtype["formats"].insert(0, np.int64)
 
         # create an empty array and copy the values
         data = np.empty(len(odata), dtype=dtype)
@@ -79,7 +104,7 @@ class ReadTile(run.Step):
     def process(self, tile):
         with open(tile.raw_file_path) as fp:
             oarr, size = self.read_dat(fp)
-        arr = self.add_columns(oarr, size, tile.id, SOURCE_DTYPE)
+        arr = self.add_columns(oarr, size, tile.name, SOURCE_DTYPE)
 
         tile.store_npy_file(arr)
         tile.size = size
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #~ python in_corral.py freshload;
-python in_corral.py run -s ReadTile ReadPawprintStack;
-python in_corral.py run -s OGLE3TagTile;
-python in_corral.py run -s PrepareForMatch;
-time python in_corral.py run -s Match;
-time python in_corral.py run -s MergeLightCurves;
-python in_corral.py enable-fe b202;
+python in_corral.py run -s ReadTile;
+#~ python in_corral.py run -s ReadPawprintStack;
+#~ python in_corral.py run -s OGLE3TagTile;
+#~ python in_corral.py run -s PrepareForMatch;
+#~ time python in_corral.py run -s Match;
+#~ time python in_corral.py run -s MergeLightCurves;
+#~ python in_corral.py enable-fe b202;
