7c088e1d95545367352d9df21a27c4f608196ba3
juanbc <jbc.develop@gmail.com>
2017-09-12 00:17:58 -0300

    fix

--- carpyncho/steps/features_extractor.py
+++ carpyncho/steps/features_extractor.py
@@ -5,6 +5,8 @@
 # IMPORTS
 # =============================================================================
 
+from __future__ import print_function
+
 import warnings
 import uuid
 
@@ -17,7 +19,7 @@ import pandas as pd
 import feets
 
 from ..models import LightCurves
-from ..lib.mppandas import mp_apply
+from ..lib.mppandas import mp_apply, CORES
 
 # =============================================================================
 # HELPER FUNCTIONS
@@ -25,21 +27,23 @@ from ..lib.mppandas import mp_apply
 
 class Extractor(object):
 
-    def __init__(self, fs, obs, tile_name):
+    def __init__(self, fs, obs, tile_name, chunkn, chunkst):
         self._fs = fs
         self._obs = obs
         self._tname = tile_name
+        self._chunkn = chunkn
+        self._chunkst = chunkst
 
     def __call__(self, srcs):
         fs = self._fs
-        self._cnt, self._total, self._uid = 1, len(srcs), uuid.uuid4()
+        self._cnt, self._total = 1, len(srcs)
         srcs[fs.features_as_array_] = srcs.id.apply(self.extract)
-        del self._cnt, self._total, self._uid
+        del self._cnt, self._total
         return srcs
 
     def extract(self, src_id):
-        print("[{}-chunk-{}] Extracting Source {}/{}...".format(
-            self._tname, self._uid, self._cnt, self._total))
+        print("[{}-chunk-{}/{}] Extracting Source {}/{}...".format(
+            self._tname, self._chunkn, self._chunkst, self._cnt, self._total))
         self._cnt += 1
 
         fs, obs = self._fs, self._obs
@@ -75,20 +79,24 @@ class FeaturesExtractor(run.Step):
     groups = ["fe"]
 
     min_observation = conf.settings.get("FE_MIN_OBSERVATION", 30)
-    mp_cores = conf.settings.get("FE_MP_CORES", None)
-    mp_split = conf.settings.get("FE_MP_SPLIT", None)
+    chunk_size = conf.settings.get("FE_CHUNK_SIZE", CORES * 10)
+    write_limit = conf.settings.get("FE_WRITE_LIMIT", 1000)
+    mp_cores = conf.settings.get("FE_MP_CORES", CORES)
+    mp_split = conf.settings.get("FE_MP_SPLIT", CORES)
 
     def setup(self):
         print("min_observation:", self.min_observation)
+        print("chunk_size:", self.chunk_size)
+        print("write_limit:", self.write_limit)
         print("mp_cores:", self.mp_cores)
         print("mp_split:", self.mp_split)
         self.fs = feets.FeatureSpace(
             data=["magnitude", "time", "error"],
             exclude=["SlottedA_length", "StetsonK_AC"])
-        #~ self.fs = feets.FeatureSpace(
-            #~ only=["PeriodLS", "AMP"])
 
     def get_sources(self, lc):
+        """Get the sources of the lightcurve"""
+
         # get the ids of the sources with at least min_observation matches
         cnt = lc.obs_counter
         with_min_obs = pd.DataFrame(cnt[cnt["cnt"] >= self.min_observation])
@@ -96,32 +104,37 @@ class FeaturesExtractor(run.Step):
         # extract ids and classes of ogle
         sources = pd.DataFrame(lc.tile.load_npy_file()[["id", "ogle3_type"]])
 
-        # filter with the filter :D
+        # filter with min obs
         sources = sources[sources.id.isin(with_min_obs.id)]
+
+        # filter the already used sources
+        exclude = lc.features
+        if exclude is not None:
+            sources = sources[~sources.id.isin(exclude["id"])]
 
         # add the cnt
         sources = pd.merge(sources, with_min_obs, on="id", how="inner")
 
         # free memory
-        del cnt, with_min_obs
+        del cnt, with_min_obs, exclude
 
         return sources
 
-    def get_obs(self, lc, sources):
-        obs = lc.observations
-        obs = obs[np.in1d(obs["bm_src_id"], sources.id)]
+    def get_obs(self, obs, sources_ids):
+        """Return the observations of the given sources"""
+        obs = obs[np.in1d(obs["bm_src_id"], sources_ids)]
         return obs
 
-    def process(self, lc):
-        print("Selecting sources...")
-        sources = self.get_sources(lc)
-        print("Filtering observarions...")
-        obs = self.get_obs(lc, sources)
+    def chunk_it(self, sources):
+        """Split the source in many parts to low the memory footprint in pandas
+        mp_apply
 
-        extractor = Extractor(self.fs, obs, lc.tile.name)
-        sources = mp_apply(
-            sources, extractor, procs=self.mp_cores, chunks=self.mp_split)
+        """
+        split_size = int(len(sources) / self.chunk_size)
+        chunks = np.array_split(sources, split_size)
+        return chunks
 
+    def to_recarray(self, sources):
         # convert to recarray
         sources = sources.to_records(index=False)
 
@@ -131,7 +144,56 @@ class FeaturesExtractor(run.Step):
         descr = [(str(n), t) for n, t in descr]
         dt = np.dtype(descr)
 
-        lc.features = sources.astype(dt)
-        del sources
+        return sources.astype(dt)
 
+    def write_is_needed(self, lc, features):
+        if len(features) < self.write_limit:
+            return features
+        print("Writing {} new features...".format(len(features)))
+        stored = lc.features
+        if stored is None:
+            to_store = features
+        else:
+            to_store = np.append(stored, features)
+        lc.features = to_store
+        print("{} FEATURES SET STORED!".format(len(to_store)))
+        del stored, to_store
+
+    def process(self, lc):
+        print("Selecting sources...")
+        all_sources = self.get_sources(lc)
+        print("{} SOURCES FOUND!".format(len(all_sources)))
+
+        #~ all_obs = self.get_obs(lc.observations, all_sources.id)
+
+        # chunk all the sources (the rename is for free memory
+        chunks = self.chunk_it(all_sources)
+        chunkst = len(chunks)
+
+        # free memory
+        del all_sources
+
+        features = None
+        for chunkn, sources in enumerate(chunks):
+            print("Chunk {}/{} START!".format(chunkn + 1, chunkst))
+
+            print("Filtering observarions...")
+            obs = self.get_obs(lc.observations, sources.id)
+
+            extractor = Extractor(
+                fs=self.fs, obs=obs, tile_name=lc.tile.name,
+                chunkn=chunkn + 1, chunkst=chunkst)
+
+            result = mp_apply(
+                sources, extractor, procs=self.mp_cores, chunks=self.mp_split)
+            result = self.to_recarray(result)
+
+            if features is None:
+                features = result
+            else:
+                features = np.append(features, result)
+
+            features = self.write_is_needed(lc, features)
+
+        features = self.write_is_needed(lc, features)
         self.session.commit()
